f58e39e1b16fe829209d4f65c6a8e45f7e155ce1
SJ23y <mcfolland@gmail.com>
2016-11-26 12:23:10 +0300

    Task 2. Third attempt

--- hello.py
+++ hello.py
@@ -8,4 +8,17 @@ def appl(environ, start):
 
 bind = '0.0.0.0.8080'
 command = '/etc/init.d/gunicorn hello:appl'
+workers = 5
 
+CONFIG = {
+  'mode': 'wsgi',
+  'working_dir': '/home/box/web',
+  'python': '/usr/bin/python3',
+  'args': (
+    '--bind=0.0.0.0:8080',
+    '--workers=5',
+    '--timeout=15',
+    '--log-level=debug',
+    'hello:appl'
+  )
+}
