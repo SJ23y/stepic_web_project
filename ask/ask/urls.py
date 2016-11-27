4df7509d190d3f9a53f5e7220fd9be5bf112f208
SJ23y <mcfolland@gmail.com>
2016-11-27 16:02:02 +0300

    Task 3. Attempt 3

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', qa.views.test, name='login'),
-    url(r'^signup/', qa.views.test, name='signup'),
-    url(r'^ask/', qa.views.test, name='ask'),
-    url(r'^popular/', qa.views.test, name='popular'),
-    url(r'^new/', qa.views.test, name='new'),
-    url(r'^question/(?P<id>[0-9]+)/', qa.views.test, name='question'),
-    url(r'^', qa.views.test),
+    url(r'^login/$', 'qa.views.test', name='login'),
+    url(r'^signup/$', 'qa.views.test', name='signup'),
+    url(r'^ask/.*$', 'qa.views.test', name='ask'),
+    url(r'^popular/$', 'qa.views.test', name='popular'),
+    url(r'^new/$', 'qa.views.test', name='new'),
+    url(r'^question/(?P<id>[0-9]+)/', 'qa.views.test', name='question'),
+    url(r'^$', 'qa.views.test'),
     
 ]
