56405a2025662f60221e44b14ea5cf418053f2cd
SJ23y <mcfolland@gmail.com>
2016-12-25 17:42:28 +0300

    Task 4. Create Models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    tittle = models.CharField(max_length = 255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.OneToOneField(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, null=False)
+    author = models.OneToOneField(User)
+
+class QuestionManager():
+    def new():
+        return self.order_by('-added_at')
+
+    def popular():
+        return self.order_by('-raiting')
